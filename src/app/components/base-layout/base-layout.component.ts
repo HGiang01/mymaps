fa7d17e90384c616529a920fb8ac8c2b90266b9c
CiaoFenG-04 <huynhphong55575@gmail.com>
2025-06-20 02:29:30 +0700

    Hoàn tất chức năng feedback, about-us, contact

--- src/app/components/base-layout/base-layout.component.ts
+++ src/app/components/base-layout/base-layout.component.ts
@@ -2,6 +2,9 @@ import { Component, Input, OnInit } from '@angular/core';
 import { IonicModule, MenuController } from '@ionic/angular'; 
 import { Router } from '@angular/router'; 
 import { AuthService } from '../../services/auth.service';
+import { FeedbackPage } from 'src/app/pages/feedback/feedback.page';
+import { ModalController } from '@ionic/angular';
+import { ContactPage } from 'src/app/pages/contact/contact.page';
 
 @Component({
   selector: 'app-base-layout',
@@ -19,7 +22,9 @@ export class BaseLayoutComponent implements OnInit {
   constructor(
     private menu: MenuController,
     private router: Router,
-    private authService: AuthService
+    private authService: AuthService,
+    public menuCtrl: MenuController,
+    private modalCtrl: ModalController,
   ) {};
 
   ngOnInit() {
@@ -42,6 +47,42 @@ export class BaseLayoutComponent implements OnInit {
   goToAboutUs() {
     this.menu.close('main-menu');
     this.router.navigate(['/about-us']);
+  }
+
+  goToFeedback() {
+    this.router.navigate(['/feedback']);
+    this.menuCtrl.close();
+  }
+
+  async goToContact() {
+    const modal = await this.modalCtrl.create({
+      component: ContactPage,
+      cssClass: 'contact-modal-center',
+      backdropDismiss: true,
+      showBackdrop: true,
+      animated: true
+    });
+    
+    await modal.present();
+  }
+
+  async openFeedback() {
+    const modal = await this.modalCtrl.create({
+    component: FeedbackPage,
+    cssClass: 'my-feedback-modal',
+    backdropDismiss: true,
+    showBackdrop: true,
+    animated: true
+  });
+  
+    modal.onWillDismiss().then(res => {
+      if (res.data) {
+        console.log('Feedback:', res.data);
+        // TODO: gửi về API hoặc xử lý dữ liệu
+      }
+    });
+
+    await modal.present();
   }
 
   logout() {
